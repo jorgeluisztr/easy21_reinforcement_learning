79f70073d576e5bddc29302252667b79df27f8e6
jorgeluisztr <jorgeluiszatarain@gmail.com>
2020-02-02 21:51:28 -0600

    easy21

---
+++ environment.py
@@ -0,0 +1,56 @@
+import numpy as np
+
+class easy21():
+
+    def __init__(self):
+        self.deck = np.arange(1,11)
+        self.color = np.arange(1,4)
+        self.playersum = np.random.choice(self.deck)
+        self.dealerobserved = np.random.choice(self.deck)
+        self.dealersum = self.dealerobserved
+        self.end_game = False
+        self.result = "unknown"
+
+    def state(self):
+        return self.playersum, self.dealerobserved
+
+    def hit(self):
+        if np.random.choice(self.color) > 3:
+            return np.random.choice(self.deck)
+        else:
+            return -np.random.choice(self.deck)
+
+    def is_game_end(self):
+        self.end_game = True
+
+    def turn(self, action):
+        if action == 1:
+            self.playersum = self.playersum + self.hit()
+            if self.playersum < 1 or self.playersum > 21:
+                self.is_game_end()
+                self.result = "lose"
+                return self.state(), -1
+            else:
+                return self.state(), 0
+        else:
+            while not self.end_game:
+                if self.dealersum < 1 or self.dealersum >21:
+                    self.is_game_end()
+                    self.result = "win"
+                    return self.state(), 1
+                else:
+                    if self.dealersum < 17:
+                        self.dealersum = self.hit()
+                    if self.dealersum > 16 and self.dealersum < 22:
+                        if self.dealersum < self.playersum:
+                            self.is_game_end()
+                            self.result = "win"
+                            return self.state(), 1
+                        elif self.dealersum == self.playersum:
+                            self.is_game_end()
+                            self.result = "draw"
+                            return self.state(), 0
+                        else:
+                            self.is_game_end()
+                            self.result = "lose"
+                            return self.state(), -1
---
+++ monte_carlo.py
@@ -0,0 +1,31 @@
+import numpy as np
+from environment import easy21
+
+def epsilon_greedy(Nsa, Qsa, playersum, dealerobs):
+    e = 100 / (100 + np.sum(Nsa[playersum -1, dealerobs -1, :]))
+    if np.random.uniform(0,1) > e:
+        action = np.argmax(Qsa[playersum-1, dealerobs-1])
+    else:
+        action = np.random.choice([0,1])
+    return action
+
+def monte_carlo(limit_stop):
+    Qsa = np.zeros([21,10,2])
+    Nsa = np.zeros([21,10,2])
+    for i in range(limit_stop):
+        episode = easy21()
+        playersum, dealerobs = episode.state()
+        history = []
+        while not episode.end_game:
+            action = epsilon_greedy(Nsa, Qsa, playersum, dealerobs)
+            Nsa[playersum - 1, dealerobs - 1, action] += 1
+            state, reward = episode.turn(action)
+            history.append(([playersum, dealerobs], action, reward))
+            playersum, dealerobs = state
+        Gt = 0
+        for j, (State, Action, Reward) in enumerate(reversed(history)):
+            playersum, dealerobs = State
+            alpha = 1/Nsa[playersum - 1, dealerobs - 1, Action]
+            Gt = Gt + Reward
+            Qsa[playersum-1, dealerobs-1, Action] += alpha*(Gt - Qsa[playersum-1, dealerobs-1, Action])
+    return Qsa
